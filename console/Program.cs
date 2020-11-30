b3c6c86e8f260212d453982726bc6f0b22028f51
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-12-01 09:14:39 +1100

    initial commit

---
+++ console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
